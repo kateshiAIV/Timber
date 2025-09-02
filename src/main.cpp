61a628633e8d61ff9bcf83dd5e4c733d259e9cff
kateshiAIV <148715019+kateshiAIV@users.noreply.github.com>
2025-09-02 15:52:25 +0200

    background displaying

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <SFML/Graphics.hpp>
 
 
@@ -6,22 +7,29 @@ using namespace sf;
 
 
 
+
 int main()
 {
     VideoMode vm({ 1920u, 1080u });
     RenderWindow window = RenderWindow(vm, "Timberman");
+	Texture textureBackground;
+	textureBackground.loadFromFile("graphics/background.png");
+	Sprite spriteBackground(textureBackground);
+    spriteBackground.setPosition(sf::Vector2<float>(0.0f,0.0f));
+
+
+    std::ofstream("file.txt") << "file content";
 
     while (window.isOpen()) {
-
-
         if (Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) {
             window.close();
         }
 
-
-
-
+        window.clear();                       // очистка окна
+        window.draw(spriteBackground);        // рисуем фон
+        window.display();                     // выводим на экран
     }
+
     
     return 0;
 }
